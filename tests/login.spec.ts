c5add5b990871b29e2ffe493199cf309772bd4a2
vishnuvandana28 <vandana28sep@gmail.com>
2026-09-15 11:02:31 +1000

    Added login scenarios

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,1 +1,3 @@
-console.log('initial login code from team member1');
+console.log("initial login code from team member1");
+console.log("Valid login testadded by Team member2");
+console.log("Invalid login testadded by Team member2");
